c2c272af0336ecfa7759cc5a3ea50209da089315
Ayush <ayushbansal0147@gmail.com>
2023-07-17 12:05:50 +0530

    my second commit

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './hero.css'
+const hero = () => {
+  return (
+    <div className='hero'>
+      <div className="left-h">
+    left side
+      </div>
+      <div className="right-h">
+    right side
+      </div>
+    </div>
+  )
+}
+
+export default hero
